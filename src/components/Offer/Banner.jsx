e0fc24987ca47c9e60ad5e754883b9f8ad7ea0c9
WILLSON <98232630+CAMEROU@users.noreply.github.com>
2023-01-06 00:33:55 -0800

    Update Banner.jsx

--- src/components/Offer/Banner.jsx
+++ src/components/Offer/Banner.jsx
@@ -3,6 +3,7 @@ import "../../style/main.scss";
 
 export default function Banner() {
 	return (
+		
 		<div className="banner-container">
 			<div className="banner-text-container">
 				<div className="banner-text">Chez vous, partout et ailleurs</div>
